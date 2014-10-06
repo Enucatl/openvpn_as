5281bc12bbfcbdaf348a56e03e384bdd858150e5
Matteo Abis <gmatteo..abis@gmail.com>
2014-10-06 14:10:24 +0200

    downloads openvpn

---
+++ README.md
@@ -0,0 +1,9 @@
+Install openvpn as
+=======================
+
+Features
+-----
+
+* install `openvpn-as`
+* setup openvpn user
+* allow vpn through the `ufw`
---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+class openvpn_as ($openvpn_location, $password) {
+    class openvpn_downloader {
+      exec { "/usr/bin/env wget -O openvpn_as.deb --timestamping $openvpn_location":
+        alias => "exec_openvpn_download",
+        cwd => "/tmp",
+      }
+
+      exec { "/usr/bin/env dpkg -i -fnoninteractive /tmp/openvpn_as.deb":
+        require => Exec["exec_openvpn_download"],
+      }
+    }
+
+    class { 'openvpn_downloader': }
+
+    user { 'openvpn':
+      ensure => "present",
+      password => $password,
+      require => Class["openvpn_downloader"]
+    }
+}
